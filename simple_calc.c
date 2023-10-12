2b0a8888952391eb852e4adb42f5a2c49083db2e
Soham251007 <64246844+Soham251007@users.noreply.github.com>
2023-10-12 22:42:55 +0530

    Add files via upload

---
+++ simple_calc.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int a, b, c;
+
+    printf("Enter the value for a \n");
+    scanf("%d", &a);
+
+    printf("Enter the value for b\n");
+    scanf("%d", &b);
+
+    printf("Addition of a and b is %d", a+b);
+
+    return 0;
+}
