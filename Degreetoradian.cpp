8f23809a437eff5bafae4b341f8a5efdf4b4e41c
Soham251007 <64246844+Soham251007@users.noreply.github.com>
2023-11-09 17:22:48 +0530

    Create Degreetoradian.cpp

---
+++ Degreetoradian.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+  double a, b;
+
+  // this is a code used to convert degrees to radians but can seem to understand how to represent the output fractionally 🥲🥲
+
+  cout<< "Enter the angle measure in degrees "<<endl;
+  cin>>a;
+  b=a*3.14/180;
+
+  cout<<"Angle measure in radians is "<<b<<endl;
+  
+}
