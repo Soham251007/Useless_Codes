9fe97f65d0720c4922380ada1da507454cc5a82f
Soham251007 <64246844+Soham251007@users.noreply.github.com>
2023-11-09 17:09:34 +0530

    Create Sq.rootcalc.cpp

---
+++ Sq.rootcalc.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+  double a, b;
+
+  // this is a code to take out sq. root which i am very bad at 😭😅
+  cout<<"enter your no. "<<endl;
+  cin>>a;
+
+  b = pow(a, 0.5);
+
+  cout<<"The sq. root of you no. is "<<b<<endl;
+  
+  return 0;
+}
