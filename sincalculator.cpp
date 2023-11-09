a342acf8f435153591d3cb49b7dc64ab9e8436bd
Soham251007 <64246844+Soham251007@users.noreply.github.com>
2023-11-09 17:51:04 +0530

    Create sincalculator.cpp

---
+++ sincalculator.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+// Made by Soham Singh please dont copy
+int main() {
+  // This is a very basic sin calculator enjoy 😁
+
+  double a, b, c;
+  cout<<"Enter the angles in degree "<<endl;
+  cin>>a;
+
+  b = a*M_PI/180;
+  c = sin(b);
+
+  cout<<"sin value is "<<c<<endl;
+  
+  return 0;
+}
+// Saale sacchi bol rha copy mat karna bkl
